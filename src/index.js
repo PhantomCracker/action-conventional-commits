e54d7589d34e07739555a4a4f842449f7825cc4c
Popa Vlad <popa.vlad94@gmail.com>
2023-07-29 10:05:16 +0300

    fix: try new methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-const {execSync} = require('child_process');
+// const {execSync} = require('child_process');
+//
+// // execSync("rm -rf ./dist");
+// execSync("rm -rf node_modules");
+// execSync("rm yarn.lock");
+// execSync("yarn cache clean");
+// execSync("yarn set version latest");
+// execSync("yarn install");
+//
+// const output = execSync("npx tsx src/main.ts");
+// console.log(output.toString());
 
-// execSync("rm -rf ./dist");
-execSync("rm -rf node_modules");
-execSync("rm yarn.lock");
-execSync("yarn cache clean");
-execSync("yarn set version latest");
-execSync("yarn install");
 
-const output = execSync("npx tsx src/main.ts");
-console.log(output.toString());
+const { exec } = require("child_process");
+
+exec("yarn install", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
