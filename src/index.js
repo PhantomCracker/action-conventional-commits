50cde0c97e5643df463a3290ac48607ae4ebad2e
Popa Vlad <popa.vlad94@gmail.com>
2023-07-29 09:47:39 +0300

    fix: add legacy peers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const {execSync} = require('child_process');
 
 execSync("rm -rf ./dist");
-execSync("yarn cache clean --force");
-execSync("yarn install --loglevel=error");
+execSync("yarn install --loglevel error --legacy-peer-deps");
 
 const output = execSync("yes 2>/dev/null | npx tsx src/main.ts");
 console.log(output.toString());
