a8d2ae79b929841365c0cfc12ffab2756670ca4d
Popa Vlad <popa.vlad94@gmail.com>
2023-07-29 09:54:09 +0300

    fix: loglevel

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ execSync("yarn set version latest");
 execSync("yarn cache clean");
 execSync("rm -rf node_modules");
 execSync("rm yarn.lock");
-execSync("yarn install --check-files");
-// execSync("yarn install --loglevel error --legacy-peer-deps");
+// execSync("yarn install --check-files");
+execSync("yarn install --loglevel error --legacy-peer-deps");
 
 const output = execSync("yes 2>/dev/null | npx tsx src/main.ts");
 console.log(output.toString());
