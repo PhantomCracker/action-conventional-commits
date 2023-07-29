134e81b78b77983593829db2e30ebcb989533927
Popa Vlad <popa.vlad94@gmail.com>
2023-07-29 09:51:18 +0300

    fix: add additional checks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const {execSync} = require('child_process');
 
 execSync("rm -rf ./dist");
-execSync("yarn install --loglevel error --legacy-peer-deps");
+execSync("yarn set version latest");
+execSync("yarn cache clean");
+execSync("rm -rf node_modules");
+execSync("rm yarn.lock");
+execSync("yarn install --check-files");
+// execSync("yarn install --loglevel error --legacy-peer-deps");
 
 const output = execSync("yes 2>/dev/null | npx tsx src/main.ts");
 console.log(output.toString());
