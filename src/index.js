ada21258f4495ae8cbf9d11808002c7f71be36cc
Popa Vlad <popa.vlad94@gmail.com>
2023-07-29 10:48:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-// const {execSync} = require('child_process');
-//
-// // execSync("rm -rf ./dist");
-// execSync("rm -rf node_modules");
-// execSync("rm yarn.lock");
-// execSync("yarn cache clean");
-// execSync("yarn set version latest");
-// execSync("yarn install");
-//
-// const output = execSync("npx tsx src/main.ts");
-// console.log(output.toString());
-
-
 const { exec, execSync } = require("child_process");
 
 exec("rm -rf ./dist", (error, stdout, stderr) => {
@@ -20,7 +7,7 @@ exec("rm -rf ./dist", (error, stdout, stderr) => {
     }
 });
 execSync("yarn install && yarn add tsx --dev");
-exec("yes 2>/dev/null | npx tsx src/main.ts", (error, stdout, stderr) => {
+exec("yes 2>/dev/null | npx tsx PhantomCracker/action-conventional-commits@master/src/main.ts", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
